bfc4dd21617a48c5c2244b96416283c287f5ce53
al-petrushin <stuartrevol@gmail.com>
2020-02-16 20:49:14 +0300

    #491: Variable in config

--- src/lib/day-calendar/day-calendar.component.ts
+++ src/lib/day-calendar/day-calendar.component.ts
@@ -35,6 +35,7 @@ import {IMonthCalendarConfig} from '../month-calendar/month-calendar-config';
 import {IMonth} from '../month-calendar/month.model';
 import {DateValidator} from '../common/types/validator.type';
 import {INavEvent} from '../common/models/navigation-event.model';
+import { CalendarMode } from '../common/types/calendar-mode';
 
 const moment = momentNs;
 
@@ -66,11 +67,32 @@ export class DayCalendarComponent implements OnInit, OnChanges, ControlValueAcce
 
   set selected(selected: Moment[]) {
     this._selected = selected;
+
+    if (!selected.length) {
+      this.monthIsSelect = false;
+    }
+
     this.onChangeCallback(this.processOnChangeCallback(selected));
   }
 
   get currentDateView(): Moment {
     return this._currentDateView;
+  }
+
+  get currentDisplayMode(): ECalendarMode {
+    if (
+      this.currentCalendarMode === this.CalendarMode.Day &&
+      (this.monthIsSelect || this.config.calendarModeDisplayFirst === 'day')
+    ) {
+      return this.CalendarMode.Day
+    }
+
+    if (
+      (this.currentCalendarMode === this.CalendarMode.Month) ||
+      (!this.monthIsSelect && this.config.calendarModeDisplayFirst === 'month')
+    ) {
+      return this.CalendarMode.Month
+    }
   }
 
   set currentDateView(current: Moment) {
@@ -104,6 +126,7 @@ export class DayCalendarComponent implements OnInit, OnChanges, ControlValueAcce
   validateFn: DateValidator;
   currentCalendarMode: ECalendarMode = ECalendarMode.Day;
   monthCalendarConfig: IMonthCalendarConfig;
+  monthIsSelect: boolean = false;
   _shouldShowCurrent: boolean = true;
   navLabel: string;
   showLeftNav: boolean;
@@ -145,6 +168,10 @@ export class DayCalendarComponent implements OnInit, OnChanges, ControlValueAcce
     this.inputValueType = this.utilsService.getInputType(this.inputValue, this.componentConfig.allowMultiSelect);
     this.monthCalendarConfig = this.dayCalendarService.getMonthCalendarConfig(this.componentConfig);
     this._shouldShowCurrent = this.shouldShowCurrent();
+
+    if (this.config.calendarModeDisplayFirst === 'day') {
+      this.monthIsSelect = true;
+    }
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -296,6 +323,7 @@ export class DayCalendarComponent implements OnInit, OnChanges, ControlValueAcce
   monthSelected(month: IMonth) {
     this.currentDateView = month.date.clone();
     this.currentCalendarMode = ECalendarMode.Day;
+    this.monthIsSelect = true;
     this.onMonthSelect.emit(month);
   }
 
